5057d5e72b4264cfb64be09b4e52a9d628195546
Dulana Peiris <dulthar@gmail.com>
2024-04-05 09:26:49 +0530

    added copy script

--- sashi_release.sh
+++ sashi_release.sh
@@ -9,4 +9,8 @@ tag_version="sashi_$1"
 target_branch="$2"
 release_note="$3"
 
+./copy_files.sh
+git add .
+git commit -m "$tag_version $release_note"
+git push origin main
 gh release create "$tag_version" ./installer/* -t "$tag_version" --target "$target_branch" -n "$release_note"
