598f42a66e46d7fd211bb7f43a2d7c4f46f428b5
Dulana Peiris <dulthar@gmail.com>
2024-07-22 19:31:51 +0530

    multi hash

--- test-file-read1.js
+++ test-file-read1.js
@@ -11,16 +11,25 @@ const fileStream = fs.createReadStream(filePath, //{ highWaterMark: 900 * 1024 *
 
 console.log('dbg: Creating hash object');
 const hash = crypto.createHash('sha512');
+const hash1 = crypto.createHash('sha512');
+const hash2 = crypto.createHash('sha512');
+const hash3 = crypto.createHash('sha512');
 
 fileStream.on('data', (chunk) => {
   console.log('dbg: Reading chunk of size', Math.floor(chunk.length / (1024 * 1024)), 'MB');
   hash.update(chunk);
+  hash1.update(chunk+"1");
+  hash2.update(chunk+"2");
+  hash3.update(chunk+"3");
   console.log('dbg: Updated hash with chunk');
 });
 
 fileStream.on('end', () => {
   console.log('dbg: File read completed');
   const hashValue = hash.digest('hex');
+  console.log("Hash1:", hash1.digest('hex'));
+  console.log("Hash2:", hash2.digest('hex'));
+  console.log("Hash3:", hash3.digest('hex'));
   console.log('dbg: Hash computed');
   console.log('Hash Value:', hashValue);
 });
