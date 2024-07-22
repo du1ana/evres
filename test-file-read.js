a2af86279a488d79a7f3a9ced9f071214010c1df
Dulana Peiris <dulthar@gmail.com>
2024-07-22 19:12:56 +0530

    test read added dbg logs

--- test-file-read.js
+++ test-file-read.js
@@ -4,21 +4,26 @@ const crypto = require('node:crypto');
 
 const filePath = path.join(__dirname, 'large_file.bin');
 
-const fileStream = fs.createReadStream(filePath, { highWaterMark: 900 * 1024 * 1024 }
+console.log('dbg: Creating file stream');
+const fileStream = fs.createReadStream(filePath, { highWaterMark: 900 * 1024 * 1024 });
 
-);
+console.log('dbg: Creating hash object');
 const hash = crypto.createHash('sha512');
 
 fileStream.on('data', (chunk) => {
-  console.log(Math.floor(chunk.length / (1024 * 1024)));
+  console.log('dbg: Reading chunk of size', Math.floor(chunk.length / (1024 * 1024)), 'MB');
   hash.update(chunk);
+  console.log('dbg: Updated hash with chunk');
 });
 
 fileStream.on('end', () => {
+  console.log('dbg: File read completed');
   const hashValue = hash.digest('hex');
-  console.log(hashValue);
+  console.log('dbg: Hash computed');
+  console.log('Hash Value:', hashValue);
 });
 
 fileStream.on('error', (err) => {
-  console.log(err);
+  console.log('dbg: Error occurred');
+  console.error(err);
 });
