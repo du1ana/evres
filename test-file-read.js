1f9a1b68366e7d6b1140ab98d8713e0615267107
Dulana Peiris <dulthar@gmail.com>
2024-07-22 19:08:23 +0530

    test read change1

--- test-file-read.js
+++ test-file-read.js
@@ -4,12 +4,14 @@ const crypto = require('node:crypto');
 
 const filePath = path.join(__dirname, 'large_file.bin');
 
-const fileStream = fs.createReadStream(filePath, { highWaterMark: 900 * 1024 * 1024 });
+const fileStream = fs.createReadStream(filePath, //{ highWaterMark: 900 * 1024 * 1024 }
+
+);
 const hash = crypto.createHash('sha512');
 
 fileStream.on('data', (chunk) => {
+  console.log(Math.floor(chunk.length / (1024 * 1024)));
   hash.update(chunk);
-  //console.log(Math.floor(chunk.length / (1024 * 1024)));
 });
 
 fileStream.on('end', () => {
